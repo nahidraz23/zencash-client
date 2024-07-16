3d9904448c577dfadd98f88e8e661543e7fed4dd
nahidraz23 <nzamanraz@gmail.com>
2024-07-16 14:14:11 +0600

    register form validation implemented

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onsubmit = data => {
+        console.log(data)
+    }
 
     return (
         <div className="h-screen flex flex-col items-center justify-center">
@@ -8,33 +18,36 @@ const Register = () => {
                 <h1 className="text-5xl">Register</h1>
             </div>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <form className="card-body">
+                <form onSubmit={handleSubmit(onsubmit)} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="email" className="input input-bordered" required />
+                        <input {...register("name")} type="text" placeholder="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input {...register("email")} type="email" placeholder="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Mobile No</span>
                         </label>
                         <div className="w-full">
-                            
-                            <input type="number" placeholder="mobile" className="input input-bordered w-full" required />
+
+                            <input {...register("mobile")} type="number" placeholder="mobile" className="input input-bordered w-full" required />
                         </div>
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Pin</span>
                         </label>
-                        <input type="number" placeholder="password" className="input input-bordered" required />
+                        <input {...register("pin", {pattern: /^\d{5}$/})} maxLength={5} type="password" placeholder="password" className="input input-bordered" required />
+                        {
+                            errors.pin && <p className="text-red-600 text-xl font-medium text-center mt-2">Pin must be 5 digit number</p>
+                        }
                     </div>
                     <div className="form-control mt-6">
                         <button className="btn btn-primary">Register</button>
